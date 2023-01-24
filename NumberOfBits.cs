b26c40597d8640cd1b2a6994b6de88f8de82841f
Митяева Анна Валерьевна <amityaeva@smartru.com>
2023-01-24 17:21:35 +0300

    Add project files.

---
+++ NumberOfBits.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class NumberOfBits
+    {
+        public int HammingWeight(uint n)
+        {
+            var sum = 0;
+
+
+        }
+    }
+}
