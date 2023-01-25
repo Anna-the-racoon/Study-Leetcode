5d68ece52021c995c4b89e2034a78dba4c683e7a
Митяева Анна Валерьевна <amityaeva@smartru.com>
2023-01-25 16:52:54 +0300

    First version of solution

--- NumberOfBits.cs
+++ NumberOfBits.cs
@@ -8,11 +8,17 @@ namespace Leetcode
 {
     public class NumberOfBits
     {
-        public int HammingWeight(uint n)
+        public int HammingWeight(uint number)
         {
             var sum = 0;
 
+            foreach (var bit in number.ToString())
+            {
+                if (bit == '1')
+                    sum += 1;
+            }
 
+            return sum;
         }
     }
 }
