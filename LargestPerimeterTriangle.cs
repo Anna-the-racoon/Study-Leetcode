1dbee138c50b9659860dea0a221b1a52fa90a7b5
Митяева Анна Валерьевна <amityaeva@smartru.com>
2023-01-26 13:19:00 +0300

    The solution falls on execution time

--- LargestPerimeterTriangle.cs
+++ LargestPerimeterTriangle.cs
@@ -1,16 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Leetcode
+﻿namespace Leetcode
 {
     public class LargestPerimeterTriangle
     {
         public int LargestPerimeter(int[] nums)
         {
-            return 0;
+            var perimeter = 0;
+
+            for (var i = 0; i < nums.Length-2; i++)
+            {
+                for (var j = i+1; j < nums.Length-1; j++)
+                {
+                    for (var k = j + 1; k < nums.Length; k++)
+                    {
+                        if (nums[i] < nums[j] + nums[k]
+                            && nums[j] < nums[i] + nums[k]
+                            && nums[k] < nums[i] + nums[j])
+                        {
+                            perimeter = Math.Max(perimeter, (nums[i] + nums[j] + nums[k]));
+                        }
+                    }
+                }
+            }
+            return perimeter;
         }
     }
 }
