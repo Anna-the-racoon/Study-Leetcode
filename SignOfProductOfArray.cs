a02958ca8f421efaa6c5674b29e2a9132db9c8c5
Митяева Анна Валерьевна <amityaeva@smartru.com>
2023-01-26 19:03:40 +0300

    First wersion of solution

---
+++ SignOfProductOfArray.cs
@@ -0,0 +1,24 @@
+﻿namespace Leetcode
+{
+    public class SignOfProductOfArray
+    {
+        public int ArraySign(int[] nums)
+        {
+            Array.Sort(nums);
+
+            var number = 0;
+            foreach (var num in nums)
+            {
+                if (num > 0)
+                    return number%2 == 0 ? 1 : -1;
+
+                if (num == 0)
+                    return 0;
+
+                number++;
+            }
+
+            return number % 2 == 0 ? 1 : -1;
+        }
+    }
+}
