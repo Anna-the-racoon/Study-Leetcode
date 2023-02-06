fc1ec2c8b5f79de674127c2350a1163fc2b567f4
Митяева Анна Валерьевна <amityaeva@smartru.com>
2023-02-06 16:07:56 +0300

    First version of the solution

---
+++ OneStringSwapCanMakeStringsEqual.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Leetcode
+{
+    public class OneStringSwapCanMakeStringsEqual
+    {
+        public bool AreAlmostEqual(string s1, string s2)
+        {
+            for (var i = 0; i < s1.Length; i++)
+            {
+                for (var j = 0; j < s1.Length; j++)
+                {
+                    var equals = s1.ToCharArray();
+
+                    equals[i] = s1[j];
+                    equals[j] = s1[i];
+
+                    var result = true;
+                    for (var k = 0; k < s2.Length; k++)
+                    {
+                        if (equals[k] != s2[k])
+                            result = false;
+                    }
+
+                    if (result)
+                        return true;
+                }
+            }
+            return false;
+        }
+    }
+}
