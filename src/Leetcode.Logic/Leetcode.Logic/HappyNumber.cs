c1e7aa7b3ddd6f3647b5472157901ba683e71138
Курицына Анна Валерьевна <wwww.anechka@gmail.com>
2024-10-07 14:49:32 +0300

    Change logic project

---
+++ src/Leetcode.Logic/Leetcode.Logic/HappyNumber.cs
@@ -0,0 +1,39 @@
+﻿namespace Leetcode;
+
+public class HappyNumber
+{
+    public bool IsHappy(int n)
+    { 
+        var result = n;
+        var resultList = new List<int>();
+        do
+        {
+            result = HappinessCalculation(result);
+
+            if (resultList.Contains(result))
+            {
+                return false;
+            }
+            resultList.Add(result);
+
+        } while (result != 1);
+
+        return true;
+    } 
+
+    public int HappinessCalculation(int n)
+    {
+        var result = 0;
+
+        while (n > 0)
+        {
+            var element = n % 10;
+
+            result += element * element;
+
+            n /= 10;
+        }
+
+        return result;
+    }
+}
